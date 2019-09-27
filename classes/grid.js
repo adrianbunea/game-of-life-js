03903fc6f29ac70172a4e084aad5b34b0b4ce1b5
adrianbunea <adrian_bunea28@yahoo.com>
2019-09-27 14:41:18 +0300

    Create grid class and test

--- classes/grid.js
+++ classes/grid.js
@@ -1,11 +1,5 @@
-var Cell = require('../classes/cell.js');
-
 class Grid {
-  constructor (columns = 10, rows = 10) {
-    this.columns = columns;
-    this.rows = rows;
-    this.cells = Array(columns).fill(Array(rows).fill(new Cell()));
-  }
+
 }
 
 module.exports = Grid;
---
+++ tests/grid-test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var Grid = require('../classes/grid.js');
+
+describe('Grid', function() {
+  describe('#constructor()', function() {
+    it('should exist', function() {
+      let grid = new Grid();
+    });
+  });
+});
