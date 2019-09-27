64577ab1d6ea90eedd6e8e7766122479ca767d29
adrianbunea <adrian_bunea28@yahoo.com>
2019-09-27 15:09:12 +0300

    Create tests for grid with params

--- tests/grid-tests.js
+++ tests/grid-tests.js
@@ -16,6 +16,16 @@ describe('Grid', function() {
     it('should have 10 rows by default', function () {
       let grid = new Grid();
       assert.equal(grid.rows, 10);
-    })
+    });
+
+    it('should have given amount of columns', function () {
+      let grid = new Grid(5, undefined);
+      assert.equal(grid.columns, 5);
+    });
+
+    it('should have given amount of rows', function () {
+      let grid = new Grid(undefined ,5);
+      assert.equal(grid.rows, 5);
+    }); 
   });
 });
