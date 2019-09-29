92529d17eade5012f0532cef26fd9ad23f5496fe
Adrian Bunea <adrian_bunea28@yahoo.com>
2019-09-29 14:01:50 +0300

    Move Game of Life tests to cell tests

--- test/cell-tests.js
+++ test/cell-tests.js
@@ -31,9 +31,46 @@ describe('Cell', function () {
       assert.equal(cell.alive, false);
     });
 
-    it('is alive if alive parameter is true', function () {
-      let cell = new Cell({ x:0, y:0 }, true);
-      assert.equal(cell.alive, true);
+    describe('dead cell', function () {
+      describe('#willRevive()', function () {
+        function willRevive(numberOfNeighbours) {
+          return numberOfNeighbours === 3;
+        }
+    
+        it('is true when there are 3 live neighbours', function () {
+          assert.equal(willRevive(3), true);
+        });
+    
+        it('is false when there are 2 live neighbours', function () {
+          assert.equal(willRevive(2), false);
+        });
+    
+        it('is false when there are 4 live neighbours', function () {
+          assert.equal(willRevive(4), false);
+        });
+      });
+    });
+
+    describe('alive cell', function () {
+      describe('#willSurvive()', function () {
+        function willSurvive (numberOfNeighbors) {
+          return (numberOfNeighbors === 2 || numberOfNeighbors === 3);
+        }
+        
+        it('is true when there are 2 live neighbours', function () {  
+          assert.equal(willSurvive(2), true);
+        });
+    
+        it('is true when there are 3 live neighbours', function () {
+          assert.equal(willSurvive(3), true);
+        });
+        
+        [0, 1, 4, 5, 6, 7, 8].forEach((neighbourCount) => {
+          it(`is false when there are ${neighbourCount} live neighbours`, function () {
+            assert.equal(willSurvive(neighbourCount), false);
+          });
+        });
+      });
     });
   });
 });
