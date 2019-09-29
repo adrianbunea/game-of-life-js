9ddfb0028f5a37617fbbec929aa214c15a8b231e
Adrian Bunea <adrian_bunea28@yahoo.com>
2019-09-29 22:47:31 +0300

    Add tests for willLive function

--- test/cell-tests.js
+++ test/cell-tests.js
@@ -9,6 +9,14 @@ describe('Cell', function () {
     }
   }
 
+  function willSurvive (numberOfNeighbors) {
+    return (numberOfNeighbors === 2 || numberOfNeighbors === 3);
+  }
+
+  function willRevive(numberOfNeighbours) {
+    return numberOfNeighbours === 3;
+  }
+
   describe('#constructor()', function () {
     describe('sets x and y correctly', function () {
       [0, 1, 2, 3].forEach((x) => {
@@ -30,46 +38,71 @@ describe('Cell', function () {
       let cell = new Cell({ x:0, y:0 });
       assert.equal(cell.alive, false);
     });
+  });
 
-    describe('dead cell', function () {
-      describe('#willRevive()', function () {
-        function willRevive(numberOfNeighbours) {
-          return numberOfNeighbours === 3;
-        }
-    
-        it('is true when there are 3 live neighbours', function () {
-          assert.equal(willRevive(3), true);
+  describe('dead cell', function () {
+    describe('#willRevive()', function () {
+      it('is true when there are 3 live neighbours', function () {
+        assert.equal(willRevive(3), true);
+      });
+  
+      it('is false when there are 2 live neighbours', function () {
+        assert.equal(willRevive(2), false);
+      });
+  
+      it('is false when there are 4 live neighbours', function () {
+        assert.equal(willRevive(4), false);
+      });
+    });
+  });
+
+  describe('alive cell', function () {
+    describe('#willSurvive()', function () {
+      it('is true when there are 2 live neighbours', function () {  
+        assert.equal(willSurvive(2), true);
+      });
+  
+      it('is true when there are 3 live neighbours', function () {
+        assert.equal(willSurvive(3), true);
+      });
+      
+      [0, 1, 4, 5, 6, 7, 8].forEach((neighbourCount) => {
+        it(`is false when there are ${neighbourCount} live neighbours`, function () {
+          assert.equal(willSurvive(neighbourCount), false);
         });
-    
-        it('is false when there are 2 live neighbours', function () {
-          assert.equal(willRevive(2), false);
-        });
-    
-        it('is false when there are 4 live neighbours', function () {
-          assert.equal(willRevive(4), false);
+      });
+    });
+  });
+
+  describe('#willLive()', function () {
+    function willLive(numberOfNeighbors, cell) {
+      // function still returns a result if cell is undefined, should add guard clause?
+      return (cell.alive ? willSurvive(numberOfNeighbors) : willRevive(numberOfNeighbors));
+    }
+
+    describe('for alive cell', function () {
+      let cell = new Cell({ x:0, y:0 }, true);
+
+      it('is true when there are 2 live neighbours', function () {
+        assert.equal(willLive(2, cell), true);
+      });
+
+      it('is true when there are 3 live neighbours', function () {
+        assert.equal(willLive(3, cell), true);
+      });
+
+      [0, 1, 4, 5, 6, 7, 8].forEach((neighbourCount) => {
+        it(`is false when there are ${neighbourCount} live neighbours`, function () {
+          assert.equal(willLive(neighbourCount, cell), false);
         });
       });
     });
 
-    describe('alive cell', function () {
-      describe('#willSurvive()', function () {
-        function willSurvive (numberOfNeighbors) {
-          return (numberOfNeighbors === 2 || numberOfNeighbors === 3);
-        }
-        
-        it('is true when there are 2 live neighbours', function () {  
-          assert.equal(willSurvive(2), true);
-        });
-    
-        it('is true when there are 3 live neighbours', function () {
-          assert.equal(willSurvive(3), true);
-        });
-        
-        [0, 1, 4, 5, 6, 7, 8].forEach((neighbourCount) => {
-          it(`is false when there are ${neighbourCount} live neighbours`, function () {
-            assert.equal(willSurvive(neighbourCount), false);
-          });
-        });
+    describe('for dead cell', function () {
+      let cell = new Cell({ x:0, y:0 });
+
+      it('is true when there are 3 live neighbors', function () {
+        assert.equal(willLive(3, cell), true);
       });
     });
   });
