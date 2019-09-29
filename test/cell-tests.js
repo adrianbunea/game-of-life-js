525b38a4ca12452b06c6e9b80570a0142ec6e799
Adrian Bunea <adrian_bunea28@yahoo.com>
2019-09-29 22:50:04 +0300

    Add all cases for willRevive function

--- test/cell-tests.js
+++ test/cell-tests.js
@@ -45,13 +45,11 @@ describe('Cell', function () {
       it('is true when there are 3 live neighbours', function () {
         assert.equal(willRevive(3), true);
       });
-  
-      it('is false when there are 2 live neighbours', function () {
-        assert.equal(willRevive(2), false);
-      });
-  
-      it('is false when there are 4 live neighbours', function () {
-        assert.equal(willRevive(4), false);
+
+      [0, 1, 2, 4, 5, 6, 7, 8].forEach((neighbourCount) => {
+        it(`it is false when there are ${neighbourCount} live neighbours`, function () {
+          assert.equal(willRevive(neighbourCount), false);
+        });
       });
     });
   });
